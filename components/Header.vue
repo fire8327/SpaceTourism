bfae8cb3f38cf676a3d95c63169ddf0c98004e6e
fire8327 <fire83274@gmail.com>
2024-06-11 12:49:34 +0300

    create howitworks

--- components/Header.vue
+++ components/Header.vue
@@ -8,6 +8,14 @@
             <nav class="flex items-center gap-6 transition-all duration-500 z-[5] max-lg:absolute max-lg:left-0 max-lg:w-full max-lg:flex-col max-lg:py-6 max-lg:backdrop-blur-xl max-lg:h-screen max-lg:border-t border-white" :class="isMenuShow ? 'max-lg:top-full' : 'max-lg:top-0 max-lg:-translate-y-full'">
                 <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full">Каталог</NuxtLink>
                 <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full">О компании</NuxtLink>
+                <div class="flex items-center gap-4">
+                    <NuxtLink to="mailto:fire83274@gmail.com">
+                        <Icon class="text-3xl text-[#D0D6F9]" name="mdi:email-variant"/>
+                    </NuxtLink>
+                    <NuxtLink to="https://t.me/fire83274" target="_blank">
+                        <Icon class="text-3xl text-[#D0D6F9]" name="ic:twotone-telegram"/>
+                    </NuxtLink>
+                </div>
             </nav>
             <button @click="isMenuShow = !isMenuShow" class="lg:hidden">
                 <Icon class="text-3xl" name="ph:list"/>
--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,56 @@
         <div class="absolute col-span-full bottom-0 w-full bg-gradient-to-t from-[#0B0D17] to-transparent h-40"></div>
     </div>
     <div class="flex flex-col gap-6">
-        <p class="text-4xl font-Cormorant">Преимущества</p>
+        <p class="text-3xl font-Cormorant uppercase tracking-widest font-semibold"><span class="text-white/50">01</span> Преимущества</p>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
             <div v-for="advantage in advantages" class="flex flex-col gap-4 border border-white/25 rounded-xl p-4 bg-white/5 transition-all duration-500 hover:scale-105 hover:z-[4]" @mouseenter="isAdvantagesShow = true"  @mouseleave="isAdvantagesShow = false">
-                <div class="flex items-center gap-4">
+                <div class="flex items-center gap-4 font-semibold">
                     <Icon class="text-3xl text-[#D0D6F9]" :name="advantage.icon"/>
-                    <p class="font-Cormorant text-2xl">{{ advantage.title }}</p>
+                    <p class="font-Cormorant uppercase text-xl tracking-widest">{{ advantage.title }}</p>
                 </div>
-                <p>{{ advantage.description }}</p>
+                <p class="opacity-50">{{ advantage.description }}</p>
             </div>
         </div>
         <div class="fixed inset-0 backdrop-blur-xl transition-all duration-500" :class="isAdvantagesShow ? 'opacity-100 z-[3]' : ' opacity-0 z-[-1]'"></div>
+    </div>
+    <div class="flex flex-col gap-6">
+        <p class="text-3xl font-Cormorant uppercase tracking-widest font-semibold"><span class="text-white/50">02</span> Как это работает</p>
+        <div class="flex items-center max-lg:flex-col gap-6">
+            <div class="flex flex-col gap-4 relative pl-2">
+                <div class="flex flex-col">
+                    <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
+                    <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">01</span> Регистрация и Бронирование</p>
+                    <p class="opacity-70">Начните свое космическое приключение с простого шага — регистрации на нашем сайте. Выберите удобную для вас дату и забронируйте полет, заполнив онлайн-форму. </p>
+                </div>
+                <div class="flex flex-col">
+                    <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
+                    <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">02</span> Подготовка</p>
+                    <p class="opacity-70">После подтверждения бронирования вы будете приглашены пройти медицинский осмотр и необходимую подготовку. Наши тренировки включают физическую подготовку, тренировки в центрифуге.</p>
+                </div>
+                <div class="flex flex-col">
+                    <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
+                    <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">03</span> Предполетная проверка</p>
+                    <p class="opacity-70">Перед полетом проводится предполетная проверка, включающая финальный осмотр оборудования, инструктаж по безопасности и контрольный медосмотр.</p>
+                </div>
+                <div class="flex flex-col">
+                    <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
+                    <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">04</span> Полет</p>
+                    <p class="opacity-70">День полета — это момент, когда ваша мечта станет реальностью. Вы сядете на борт нашего космического корабля и отправитесь в невероятное путешествие за пределы земной атмосферы.</p>
+                </div>
+                <div class="flex flex-col">
+                    <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
+                    <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">05</span> Возвращение</p>
+                    <p class="opacity-70">После незабываемого путешествия наступает время возвращения на Землю. Наши специалисты обеспечат мягкую посадку и безопасное возвращение.</p>
+                </div>
+                <div class="flex flex-col">
+                    <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
+                    <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">06</span> Поделитесь Своей Историей</p>
+                    <p class="opacity-70">Ваши впечатления важны для нас! Поделитесь своими переживаниями и фотографиями в наших социальных сетях и на сайте.</p>
+                </div>
+                <div class="absolute top-0 -left-2 bg-[#D0D6F9]/50 w-[2px] h-full"></div>
+            </div>
+            <img src="/images/howitworks/main.jpg" alt="" class="w-full lg:w-1/2 rounded-xl lg:aspect-[7/10] lg:object-cover">
+        </div>
     </div>
 </template>
 
@@ -34,17 +73,17 @@
     const advantages = [
         {
             icon: 'ion:ios-rocket',
-            title: 'Комфортные ракеты',
+            title: 'Ракеты',
             description: 'Наши ракеты обеспечивают максимальный комфорт и безопасность для вашего космического путешествия. Современные технологии и высокий уровень комфорта позволят вам наслаждаться полетом в полной мере.',
         },
         {
             icon: 'ion:ios-nutrition',
-            title: 'Космическое питание',
+            title: 'Питание',
             description: 'На борту наших ракет вас ждет разнообразное и сбалансированное питание, специально разработанное для космических путешествий. Вкусные и полезные блюда обеспечат вам энергию и хорошее самочувствие на протяжении всего полета.',
         },
         {
             icon: 'tabler:brand-github-copilot',
-            title: 'Состав пилотов',
+            title: 'Пилоты',
             description: 'Наши пилоты – это высококвалифицированные профессионалы с богатым опытом работы в космосе. Все они являются бывшими космонавтами, что гарантирует высочайший уровень безопасности и уверенность в полете.',
         },
         {
