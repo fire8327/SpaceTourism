43d6abf036aaf7026d3b9cf18ee8a75140d5c0f3
fire8327 <fire83274@gmail.com>
2024-06-11 14:38:51 +0300

    create FAQ

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@
                 <span class="font-Cormorant">SpaceTourism</span>
             </NuxtLink>
             <nav class="flex items-center gap-6 transition-all duration-500 z-[5] max-lg:absolute max-lg:left-0 max-lg:w-full max-lg:flex-col max-lg:py-6 max-lg:backdrop-blur-xl max-lg:h-screen max-lg:border-t border-white" :class="isMenuShow ? 'max-lg:top-full' : 'max-lg:top-0 max-lg:-translate-y-full'">
-                <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full">Каталог</NuxtLink>
+                <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full">Направления</NuxtLink>
                 <NuxtLink class="flex flex-col after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full">О компании</NuxtLink>
                 <div class="flex items-center gap-4">
                     <NuxtLink to="mailto:fire83274@gmail.com" class="transition-all duration-500 hover:opacity-50 w-fit">
---
+++ components/Question.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col">
+        <button @click="isAnswerShow = !isAnswerShow" class="flex items-center justify-between p-4 gap-4 border border-white/15 rounded-xl bg-white/5">
+            <span class="font-Cormorant text-2xl font-semibold tracking-widest text-left">{{ props.question }}</span>
+            <Icon class="text-3xl text-[#D0D6F9] transition-all duration-500 shrink-0" :class="{'rotate-180' : isAnswerShow}" name="material-symbols:arrow-circle-down"/>
+        </button>
+        <p class="transition-all duration-500 overflow-hidden" :class="isAnswerShow ? 'h-44 sm:h-32 lg:h-20 p-4' : 'h-0'">{{ props.answer }}</p>
+    </div>
+</template>
+
+<script setup>
+    /* показ ответа */
+    const isAnswerShow = ref(false)
+
+
+    /* пропсы */
+    const props = defineProps({
+        question: String,
+        answer: String
+    })
+</script>
