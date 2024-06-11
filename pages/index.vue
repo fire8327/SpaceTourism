98a84cc29065bef0b5580d6c86dfaf5488090d0c
fire8327 <fire83274@gmail.com>
2024-06-11 13:09:50 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -76,7 +76,7 @@
     <div class="flex flex-col gap-6">
         <p class="text-3xl font-Cormorant uppercase tracking-widest font-semibold"><span class="text-white/50">03</span> Галерея</p>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-            <img :src="`/images/gallery/${n}.jpg`" v-for="n in 6" alt="" class="aspect-video object-cover rounded-xl transition-all duration-500 hover:scale-150 hover:z-[4]" @mouseenter="isOverlayShow = true"  @mouseleave="isOverlayShow = false">
+            <img :src="`/images/gallery/${n}.jpg`" v-for="n in 6" alt="" class="aspect-video object-cover rounded-xl transition-all duration-500 max-lg:hover:rotate-90 hover:scale-150 hover:z-[4]" @mouseenter="isOverlayShow = true"  @mouseleave="isOverlayShow = false">
             <div class="fixed inset-0 backdrop-blur-xl transition-all duration-500" :class="isOverlayShow ? 'opacity-100 z-[3]' : ' opacity-0 z-[-1]'"></div>
         </div>
         <NuxtLink class="self-end rounded-full overflow-hidden border-0 border-white/30 hover:border-[20px] font-Cormorant text-[#0B0D17] w-fit uppercase transition-all duration-500">
