60eafb5823d0261a5accfcb7f1db1bdca5bd9c22
fire8327 <fire83274@gmail.com>
2024-06-11 14:50:55 +0300

    create footer

---
+++ components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="w-full min-h-[400px] bg-[url(/images/footer/main.png)] bg-cover relative grid-container">
+        <div class="absolute col-span-full top-0 w-full bg-gradient-to-b from-[#0B0D17] to-transparent h-32"></div>
+        
+    </footer>
+</template>
+
+<script setup>
+
+</script>
