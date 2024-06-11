718e9c31ac77636862f0422ef8a9fb349a52e326
fire8327 <fire83274@gmail.com>
2024-06-11 13:05:35 +0300

    create gallery

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<template>
+<template>    
     <div class="col-span-full w-full bg-[url(/images/hero/index.jpg)] bg-cover min-h-[calc(100vh-80px)] relative grid-container">
         <div class="absolute col-span-full top-0 w-full bg-gradient-to-b from-[#0B0D17] to-transparent h-40"></div>
         <div class="absolute col-span-full inset-0 bg-[#0B0D17]/30"></div>
@@ -12,10 +12,19 @@
         </div>
         <div class="absolute col-span-full bottom-0 w-full bg-gradient-to-t from-[#0B0D17] to-transparent h-40"></div>
     </div>
+    <div class="flex items-center max-lg:flex-col gap-6">
+        <img src="/images/firstblock/main.jpg" alt="" class="w-full lg:w-1/2 rounded-xl aspect-video object-cover">
+        <div class="flex flex-col gap-4 w-full lg:w-1/2">
+            <p class="text-3xl font-Cormorant">Надоело летать в отпуск за границу? Как на счет того, чтобы слетать на Луну?</p>
+            <p class="opacity-50">Устали от пакетных туров и пляжного отдыха? Полотенца и магнитики, которые вы привозите из отпуска уже не радуют ваших близких? В следующий раз привезите им настоящий лунный камень!</p>
+            <p class="opacity-50">Мы предлагаем инновационную услугу на туристическом рынке — космический туризм.</p>
+            <p class="opacity-50">Берите отпуск и отправляйтесь с нами в полет!</p>
+        </div>   
+    </div>
     <div class="flex flex-col gap-6">
         <p class="text-3xl font-Cormorant uppercase tracking-widest font-semibold"><span class="text-white/50">01</span> Преимущества</p>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-            <div v-for="advantage in advantages" class="flex flex-col gap-4 border border-white/25 rounded-xl p-4 bg-white/5 transition-all duration-500 hover:scale-105 hover:z-[4]" @mouseenter="isAdvantagesShow = true"  @mouseleave="isAdvantagesShow = false">
+            <div v-for="advantage in advantages" class="flex flex-col gap-4 border border-white/25 rounded-xl p-4 bg-white/5 transition-all duration-500 hover:scale-105 hover:z-[4]" @mouseenter="isOverlayShow = true"  @mouseleave="isOverlayShow = false">
                 <div class="flex items-center gap-4 font-semibold">
                     <Icon class="text-3xl text-[#D0D6F9]" :name="advantage.icon"/>
                     <p class="font-Cormorant uppercase text-xl tracking-widest">{{ advantage.title }}</p>
@@ -23,12 +32,12 @@
                 <p class="opacity-50">{{ advantage.description }}</p>
             </div>
         </div>
-        <div class="fixed inset-0 backdrop-blur-xl transition-all duration-500" :class="isAdvantagesShow ? 'opacity-100 z-[3]' : ' opacity-0 z-[-1]'"></div>
+        <div class="fixed inset-0 backdrop-blur-xl transition-all duration-500" :class="isOverlayShow ? 'opacity-100 z-[3]' : ' opacity-0 z-[-1]'"></div>
     </div>
     <div class="flex flex-col gap-6">
         <p class="text-3xl font-Cormorant uppercase tracking-widest font-semibold"><span class="text-white/50">02</span> Как это работает</p>
         <div class="flex items-center max-lg:flex-col gap-6">
-            <div class="flex flex-col gap-4 relative pl-2">
+            <div class="flex flex-col gap-4 relative pl-2 w-full lg:w-1/2">
                 <div class="flex flex-col">
                     <div class="rounded-full w-2 h-2 bg-[#D0D6F9] -ml-[19px] mt-1"></div>
                     <p class="font-Cormorant uppercase tracking-widest font-semibold"><span class="text-[#D0D6F9]">01</span> Регистрация и Бронирование</p>
@@ -64,12 +73,24 @@
             <img src="/images/howitworks/main.jpg" alt="" class="w-full lg:w-1/2 rounded-xl lg:aspect-[7/10] lg:object-cover">
         </div>
     </div>
+    <div class="flex flex-col gap-6">
+        <p class="text-3xl font-Cormorant uppercase tracking-widest font-semibold"><span class="text-white/50">03</span> Галерея</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+            <img :src="`/images/gallery/${n}.jpg`" v-for="n in 6" alt="" class="aspect-video object-cover rounded-xl transition-all duration-500 hover:scale-150 hover:z-[4]" @mouseenter="isOverlayShow = true"  @mouseleave="isOverlayShow = false">
+            <div class="fixed inset-0 backdrop-blur-xl transition-all duration-500" :class="isOverlayShow ? 'opacity-100 z-[3]' : ' opacity-0 z-[-1]'"></div>
+        </div>
+        <NuxtLink class="self-end rounded-full overflow-hidden border-0 border-white/30 hover:border-[20px] font-Cormorant text-[#0B0D17] w-fit uppercase transition-all duration-500">
+            <span class="bg-white px-4 py-2">Посмотреть ещё</span>
+        </NuxtLink>
+    </div>
 </template>
 
 <script setup>
-    /* преимущества */
-    const isAdvantagesShow = ref(false)
+    /* оверлей */
+    const isOverlayShow = ref(false)
 
+    
+    /* преимущества */
     const advantages = [
         {
             icon: 'ion:ios-rocket',
