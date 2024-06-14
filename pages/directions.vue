82d958c397f05b6ff6f47dd0420f0c0fcec26775
fire8327 <fire83274@gmail.com>
2024-06-14 21:28:30 +0300

    upd directions

--- pages/directions.vue
+++ pages/directions.vue
@@ -19,9 +19,9 @@
             <div class="overflow-hidden group w-full mx-auto" :class="{'md:w-3/4 lg:w-1/2 xl:w-1/3' : isFlex}">
                 <img :src="`https://kruhlafaexwyzkfbdwki.supabase.co/storage/v1/object/public/images/planets/${planet.image}`" alt="" class="w-full aspect-square object-cover transition-all duration-500 group-hover:scale-110">
             </div>
-            <div class="flex flex-col gap-6 w-full" :class="{'lg:w-1/2 xl:w-2/3' : isFlex}">
+            <div class="flex flex-col gap-6 w-full grow" :class="{'lg:w-1/2 xl:w-2/3' : isFlex}">
                 <p class="text-5xl font-Cormorant tracking-widest font-semibold">{{ planet.name }}</p>
-                <p class="opacity-50 tracking-widest">{{ planet.desc }}</p>
+                <p class="opacity-50 tracking-widest" :class="{'grow' : !isFlex}">{{ planet.desc }}</p>
                 <div class="w-full h-px bg-white/15"></div>
                 <div class="gap-6" :class="isFlex ? 'flex items-center max-md:flex-col md:justify-center md:gap-8 xl:gap-16' : 'flex flex-col'">
                     <div class="flex flex-col gap-2 items-center text-center">
@@ -30,7 +30,7 @@
                     </div>
                     <div class="flex flex-col gap-2 items-center text-center">
                         <p class="opacity-50 uppercase text-xs">Среднее расстояние</p>
-                        <p class="font-Cormorant tracking-widest font-semibold text-4xl">{{ planet.distance.toLocaleString() }} км</p>
+                        <p class="font-Cormorant tracking-widest font-semibold text-4xl">{{ formatNumber(planet.distance) }} км</p>
                     </div>
                     <div class="flex flex-col gap-2 items-center text-center">
                         <p class="opacity-50 uppercase text-xs">Время в пути</p>
@@ -41,8 +41,8 @@
                         <p class="font-Cormorant tracking-widest font-semibold text-4xl">{{ planet.complexity }}</p>
                     </div>
                 </div>
-                <div class="flex flex-col gap-4">
-                    <p class="text-3xl uppercase font-semibold tracking-wide">{{ planet.price.toLocaleString() }} ₽</p>
+                <div class="flex flex-col gap-4" :class="{'mt-auto' : !isFlex}">
+                    <p class="text-3xl uppercase font-semibold tracking-wide">{{ formatNumber(planet.price) }} ₽</p>
                     <button class="px-4 py-0.5 rounded-full hover:bg-white/5 h-8 text-[#0B0D17] w-[260px] font-Cormorant uppercase relative overflow-hidden group">
                         <span class="transition-all duration-700 bg-white absolute inset-0 group-hover:-translate-x-full rounded-xl text-center">Оставить заявку</span>
                         <span class="absolute inset-0 text-white transition-all duration-700 translate-x-full group-hover:translate-x-0 z-[2] text-center">Оставить заявку</span>
@@ -56,6 +56,22 @@
 <script setup>
     /* расположение */
     const isFlex = ref(true)
+
+
+    /* окргуление цен */
+    const formatNumber = (number) => {
+        if (number >= 1e12) {
+            return (number / 1e12).toFixed(2) + " трлн.";
+        } else if (number >= 1e9) {
+            return (number / 1e9).toFixed(2) + " млрд.";
+        } else if (number >= 1e6) {
+            return (number / 1e6).toFixed(2) + " млн.";
+        } else if (number >= 1e3) {
+            return (number / 1e3).toFixed(2) + " тыс.";
+        } else {
+            return number.toString();
+        }
+    }
 
 
     /* подключение БД */
