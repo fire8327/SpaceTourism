a3d1fdefdecd27a37ce5ce5ceb6fe2e981c76eb4
fire8327 <fire83274@gmail.com>
2024-06-15 07:16:18 +0300

    upd headeer

--- components/Header.vue
+++ components/Header.vue
@@ -5,25 +5,32 @@
                 <img src="/images/header/logo.png" alt="" class="w-8">
                 <span class="font-Cormorant">SpaceTourism</span>
             </NuxtLink>
-            <nav class="flex items-center gap-6 transition-all duration-500 z-[5] max-lg:absolute max-lg:left-0 max-lg:w-full max-lg:flex-col max-lg:py-6 max-lg:backdrop-blur-xl max-lg:h-screen max-lg:border-t border-white" :class="isMenuShow ? 'max-lg:top-full' : 'max-lg:top-0 max-lg:-translate-y-full'">
-                <NuxtLink to="/" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">00</span> Главная</NuxtLink>
-                <NuxtLink to="/directions" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">01</span> Направления</NuxtLink>
-                <NuxtLink to="/about" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">02</span> О компании</NuxtLink>
-                <NuxtLink to="/about" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">03</span> Контакты</NuxtLink>
-                <NuxtLink to="/gallery" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">04</span> Галерея</NuxtLink>
-                <div class="flex items-center gap-4">
-                    <NuxtLink to="mailto:fire83274@gmail.com" class="transition-all duration-500 hover:opacity-50 w-fit">
-                        <Icon class="text-3xl text-[#D0D6F9]" name="mdi:email-variant"/>
-                    </NuxtLink>
-                    <NuxtLink to="https://t.me/fire83274" target="_blank" class="transition-all duration-500 hover:opacity-50 w-fit">
-                        <Icon class="text-3xl text-[#D0D6F9]" name="ic:twotone-telegram"/>
-                    </NuxtLink>
+            <nav class="flex transition-all duration-500 z-[5] w-full md:w-1/2 fixed left-0 top-0 h-screen backdrop-blur-xl" :class="{'-translate-x-full' : !isMenuShow}">
+                <button @click="isMenuShow = false" class="absolute top-6 right-6">
+                    <Icon class="text-3xl text-[#D0D6F9]" name="hugeicons:cancel-01"/>
+                </button>
+                <div class="flex flex-col gap-6 grow items-center justify-center">
+                    <NuxtLink to="/" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">00</span> Главная</NuxtLink>
+                    <NuxtLink to="/directions" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">01</span> Направления</NuxtLink>
+                    <NuxtLink to="/about" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">02</span> О компании</NuxtLink>
+                    <NuxtLink to="/about" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">03</span> Контакты</NuxtLink>
+                    <NuxtLink to="/gallery" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">04</span> Галерея</NuxtLink>
+                    <NuxtLink to="/gallery" class="relative after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-white after:transition-all after:duration-500 hover:after:w-full"><span class="font-semibold">05</span> Подготовка к полёту</NuxtLink>
                 </div>
             </nav>
-            <button @click="isMenuShow = !isMenuShow" class="lg:hidden">
-                <Icon class="text-3xl" name="ph:list"/>
-            </button>
+            <div class="flex items-center gap-4">
+                <NuxtLink to="mailto:fire83274@gmail.com" class="transition-all duration-500 hover:opacity-50 w-fit">
+                    <Icon class="text-3xl text-[#D0D6F9]" name="mdi:email-variant"/>
+                </NuxtLink>
+                <NuxtLink to="https://t.me/fire83274" target="_blank" class="transition-all duration-500 hover:opacity-50 w-fit">
+                    <Icon class="text-3xl text-[#D0D6F9]" name="ic:twotone-telegram"/>
+                </NuxtLink>
+                <button @click="isMenuShow = !isMenuShow" class="focus:outline-none">
+                    <Icon class="text-3xl text-[#D0D6F9]" name="ph:list"/>
+                </button>
+            </div>
         </div>
+        <div @click="isMenuShow = false" class="fixed w-1/2 h-screen top-0 right-0 z-[4] max-md:hidden" :class="{'hidden' : !isMenuShow}"></div>
     </header>
 </template>
 
@@ -36,6 +43,16 @@
     const nuxtApp = useNuxtApp()
     nuxtApp.hook('page:start', () => {
         isMenuShow.value = false
+    })
+
+
+    /* клавиша esc */
+    onMounted(() => {
+        window.addEventListener('keydown', (event) => {
+            if (event.key === 'Escape') {
+                isMenuShow.value = false
+            }
+        })
     })
 </script>
 
