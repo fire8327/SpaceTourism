171490e102979c44d7102f14b4230c388b0096da
fire8327 <fire83274@gmail.com>
2024-06-15 20:15:18 +0300

    upd

--- pages/preparing.vue
+++ pages/preparing.vue
@@ -5,25 +5,25 @@
             <div class="flex flex-col gap-4 w-full lg:w-1/2">
                 <p class="font-Cormorant tracking-widest font-semibold text-2xl">Не забудьте взять с собой:</p>
                 <ul class="flex flex-col gap-2 list-inside list-disc marker:text-[#D0D6F9] text-white/50">
-                    <li><span class="font-semibold text-white">Паспорт.</span> Визовый режим пока не введен, но на границе будет досмотр.</li>
-                    <li><span class="font-semibold text-white">Тяжелые ботинки.</span> Пригодятся вам в условиях низкой гравитации.</li>
-                    <li><span class="font-semibold text-white">Криптовалюта.</span> Рекомендуем заранее обменять немного Космо-коинов, чтобы купить сувениры.</li>
-                    <li><span class="font-semibold text-white">Солнцезащитные очки.</span> В космосе нет атмосферы, поэтому солнечные лучи могут быть особенно яркими и вредными для глаз.</li>
-                    <li><span class="font-semibold text-white">Аптечка.</span> Возьмите базовый набор медикаментов на случай небольших травм или недомоганий.</li>
-                    <li><span class="font-semibold text-white">Камера.</span> Запечатлейте удивительные виды и моменты вашего космического путешествия.</li>
-                    <li><span class="font-semibold text-white">Ручка и блокнот.</span> Полезно для заметок, зарисовок и ведения личного дневника экспедиции.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Паспорт.</span> Визовый режим пока не введен, но на границе будет досмотр.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Тяжелые ботинки.</span> Пригодятся вам в условиях низкой гравитации.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Криптовалюта.</span> Рекомендуем заранее обменять немного Космо-коинов, чтобы купить сувениры.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Солнцезащитные очки.</span> В космосе нет атмосферы, поэтому солнечные лучи могут быть особенно яркими и вредными для глаз.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Аптечка.</span> Возьмите базовый набор медикаментов на случай небольших травм или недомоганий.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Камера.</span> Запечатлейте удивительные виды и моменты вашего космического путешествия.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Ручка и блокнот.</span> Полезно для заметок, зарисовок и ведения личного дневника экспедиции.</li>
                 </ul>
             </div>
             <div class="flex flex-col gap-4 w-full lg:w-1/2">
                 <p class="font-Cormorant tracking-widest font-semibold text-2xl">Это лучше оставить дома:</p>
                 <ul class="flex flex-col gap-2 list-inside list-disc marker:text-[#D0D6F9] text-white/50">
-                    <li><span class="font-semibold text-white">Телефон.</span> В космосе не ловит ваш любимый оператор и нет Wi-Fi.</li>
-                    <li><span class="font-semibold text-white">Бутерброды.</span> На борту будет питание, которое не разлетится по кабине в условиях невесомости.</li>
-                    <li><span class="font-semibold text-white">Плавки/купальник.</span> Море Ясности и Океан Бурь — это большие кратеры. Без воды.</li>
-                    <li><span class="font-semibold text-white">Зонт.</span> В космосе нет дождя, так что он не понадобится.</li>
-                    <li><span class="font-semibold text-white">Косметика.</span> В условиях космоса вам не потребуется ежедневный макияж.</li>
-                    <li><span class="font-semibold text-white">Бижутерия.</span> Дополнительные украшения могут только мешать и занимать место.</li>
-                    <li><span class="font-semibold text-white">Любимая книга.</span> На борту корабля будет цифровая библиотека с большим выбором литературы.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Телефон.</span> В космосе не ловит ваш любимый оператор и нет Wi-Fi.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Бутерброды.</span> На борту будет питание, которое не разлетится по кабине в условиях невесомости.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Плавки/купальник.</span> Море Ясности и Океан Бурь — это большие кратеры. Без воды.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Зонт.</span> В космосе нет дождя, так что он не понадобится.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Косметика.</span> В условиях космоса вам не потребуется ежедневный макияж.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Бижутерия.</span> Дополнительные украшения могут только мешать и занимать место.</li>
+                    <li><span class="font-semibold text-white font-Cormorant">Любимая книга.</span> На борту корабля будет цифровая библиотека с большим выбором литературы.</li>
                 </ul>
             </div>
         </div>
